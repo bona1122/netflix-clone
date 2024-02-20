222b1bd824c576221802da88cc2d10041ea3e059
bona1122 <vnclfjqm12@gmail.com>
2024-02-20 21:53:13 +0900

    Animation: variants

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Wrapper = styled.div`
   display: flex;
@@ -10,17 +11,24 @@ const Wrapper = styled.div`
   align-items: center;
   height: 100vh;
 `;
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
   border-radius: 10px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
+
+const variance = {
+  start: { scale: 0 },
+  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
+};
 function App() {
   return (
     <Wrapper>
-      <Box />
+      <Box variants={variance} initial="start" animate="end">
+        hello
+      </Box>
     </Wrapper>
   );
 }
