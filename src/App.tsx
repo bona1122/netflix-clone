1b5b53ae50d85abe0c02d436a8e0c97fb1b1e2d3
bona1122 <vnclfjqm12@gmail.com>
2024-02-20 22:41:39 +0900

    Animation: drag control

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   display: flex;
@@ -19,22 +20,37 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const boxVariants = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  tap: { scale: 1, borderRadius: "100px" },
-  drag: { backgroundColor: "rgb(46,204,113)", transition: { duration: 10 } },
+  hover: { rotateZ: 90 },
+  tap: { borderRadius: "100px" },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box
-        drag
-        variants={boxVariants}
-        whileDrag="drag"
-        whileHover="hover"
-        whileTap="tap"
-      />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragElastic={0.5}
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileDrag="drag"
+          whileHover="hover"
+          whileTap="tap"
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
